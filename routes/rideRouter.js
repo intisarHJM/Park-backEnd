6e8fef5e9bf88fc404553280e9e52033cbe19c32
Intisar Hussain Jaafar <Intesarabdullah67@gmail.com>
2026-04-14 10:03:13 +0300

    middleware is ready

--- routes/rideRouter.js
+++ routes/rideRouter.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const rideController = require("../controllers/rideController")
+const { validateRide } = require("../middleware")
 
 router.get("/rides", rideController.getAllRides) //show all rides
 router.post("/newRide", rideController.createRide) // create/add new ride
+
+//middleware route
+router.post("/", validateRide, recipeController.createRecipe)
 
 module.exports = router
